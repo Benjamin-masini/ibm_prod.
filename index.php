2d6f5f48899a5019bc47a0456da24c1125abcafa
Benjamin Masini <142990404+Benjamin-masini@users.noreply.github.com>
2024-09-18 03:16:16 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="fr-FR">
+  <head>
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=devise-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet"type="text/css"href="BEBOL/Accueil1.CSS"/>
+    <link rel="shortcut icon" type="image/x-icon" href="BEBOL/PHOTOS/IBMprodV.png">
+    <title>IBMprod</title>
+  </head>
+  <body>
+    <h1>
+      <header class="header">
+        
+            
+                            <!--Barre de navigation-->
+            
+        <nav class="Navigation">
+          <div class="nav-menu">
+            <a class="logo" href="#"><img src="BEBOL/PHOTOS/IBMprodS.png" alt="logo de l'entreprise"></a>
+            
+          </div>
+          <ul class="menu">
+            
+            <li><a href="BEBOL/Cours.php">Cours</a></li> 
+            <li><a href="BEBOL/Publicité.php">Marketing</a></li>
+            <li><a href="BEBOL/Galerie.php">Galerie</a></li> 
+            <li><a href="BEBOL/Contacts.php">Contacts</a></li>
+            <li><a href="BEBOL/À propos .docx">À propos</a></li>
+                
+          </ul>
+          
+                            <!-- Boutton long-->
+
+          <button class="button" aria-label="Toggle navigation" aria-expanded="false">
+            <span class="menu-icone"></span>
+            <span class="menu-icone"></span>
+          </button>
+          
+        </nav>      
+      </header>
+    </h1>
+                                    <!--Barre de navigation-->
+    <h2>
+      <div class="bienvenu">BIENVENU(E)!</div>
+        <hr> 
+      <div class="service">
+        <p>Merci d'avoir choisi "BEBOL" comme votre assistant web,de faire confiance à l'entréprise IBMprod et de bien vouloir traiter un partenariat commercial.
+          <br>Ce pendant nous vous demandons de lire minutieusement notre <a href="BEBOL/Politique commerciale.pdf">Consigne d'utilisasion</a> avant de conclure un accord pour un projet,car l'entreprise qui est à caractère commercial ne traite  et conclue des accords et/ou contrats qu'avec des adultes (+18 ans)
+        </p>
+        <p>Toute fois,IBMprod ne ferme jamais ces portes aux mineurs (-18 ans).si l'utilisateur est mineur,il doit être accompagné de son tuteur pour un contrat. 
+          En ce qui concerne notre emploie du temps,nos portes sont ouverte de 09H à 18H:<br>lundi<br>Mardi<br>Mercredi<br>Jeudi<br>Vendredi<br>Sameedi(09H à 15H)
+        </p>
+        <p>Pour mieux  vous servir de près ou de loin,IBMprod met à votre disposition l'accès facil à nos services.</p>
+      </div>
+    </h2>
+    <audio class="son" controls="controls" loop="loop" autoplay="autoplay" >
+      <source src="BEBOL/SONS/i_can_t_help_it_michael_jackson_mp3_3499.mp3">
+      <source src="BEBOL/SONS/i_can_t_help_it_michael_jackson_mp3_3499.ogg">
+    </audio>
+
+  </body>
+
+                   <!--FOOTER veut dire bas.texte du bas de la page-->
+
+  <footer>
+    <p1>IBMprod45@gmail.com   <pA>IBM_prod  <pB> +243853193403</p>
+    <p2>copyright &copy;BEBOL2022 par IBMprod</p>
+  </footer>
+</html>
