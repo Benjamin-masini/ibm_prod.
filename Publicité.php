a5ae87c0ee12fe854387d02019423f682e27439f
Benjamin Masini <142990404+Benjamin-masini@users.noreply.github.com>
2024-09-18 03:37:01 +0100

    Add files via upload

---
+++ Publicité.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="fr-FR">
+    <head>
+        <meta charset="UTF-8"/>
+        <meta name="viewport" content="width=devise-width, initial-scale=1.0">
+        <title>IBMprod</title>
+        <link rel="stylesheet"type="text/css"href="Publicité.css"/>
+    </head>
+    <body>
+        
+        <h1>Voulez-vous une promotion digne de vos produits?</h1>
+        <h2 class="Details">
+            <p>Plus besoin de vous inquiétez!!<br>L’entreprise IBMprod par son département Publicitaire IBMsango a mise à la disposition de ses utilisateurs et autres  vendeurs un espace publicitaire pour la promotion de leurs produits de vente.</p>
+            <p>Alors c'est simple,vous vendez en ligne ou en physique; grossiste ou détaillant,prenez juste un abonnement (Journalier, hebdomadaire et mensuel)<br>Les annonces se feront dans divers plateformes, telles que : YouTube, Meta, Tik-tok et sur le site</p>
+            
+            <li>Mensuel: 40$</li>
+            <li>Hebdomadaire: 10$</li>
+            <li>Journalier: 3$</li>
+            <p>Prenez votre abonnement en contactant le service<a href="Contacts.php">ICI</a></p>
+            </p>
+
+        </h2>
+        
+    </body>
+
+
+
+
+    <footer>
+        <p1>IBMprod45@gmail.com   <pA>IBM_prod  <pB> +243853193403</p>
+        <p2>copyright &copy;BEBOL2022 par IBMprod</p>
+   </footer>
+</html>
